d077600aa07ff4b0ebdc37f6a9f24680ea958575
Selena Singleton <ssingle7@gmail.com>
2020-12-09 20:34:13 -0500

    copied instructions into the README.md and added variables

--- script.js
+++ script.js
@@ -6,12 +6,54 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
   passwordText.value = password;
 
 }
 
 function generatePassword(){
   return "temporary password";
+
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var numCharacters = "1234567890"
+  var specialCharacters = "!@#$%^&*()"
+
+  var requiredCharatcerString = ""
+
+  var includeUpperCase = confirm("Do you want to include uppercase character?");
+  console.log("Include uppercase characters: "=includeUpperCase); 
+  
+
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// ```
+// // GIVEN I need a new, secure password
+// // WHEN I click the button to generate a password
+// // THEN I am presented with a series of prompts for password criteria
+// // WHEN prompted for password criteria
+// // THEN I select which criteria to include in the password
+// // WHEN prompted for the length of the password
+// // THEN I choose a length of at least 8 characters and no more than 128 characters
+// // WHEN prompted for character types to include in the password
+// // THEN I choose lowercase, uppercase, numeric, and/or special characters
+// // WHEN I answer each prompts
+// // THEN my input should be validated and at least one character type should be selected
+// // WHEN all prompts are answered
+// // THEN a password is generated that matches the selected criteria
+// // WHEN the password is generated
+// // THEN the password is either displayed in an alert or written to the page
+// ```
+
+
+//length of user password
+//criteria for user password 
+//password of eight characters, no more than 128 characters 
+//
+
+
+var passwordLength = 
